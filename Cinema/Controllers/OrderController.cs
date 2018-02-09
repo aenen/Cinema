9d2ab407240726828ccc6f974dfd832a27cd451a
aenen <yaroslav.golyuk@yandex.ua>
2018-02-09 20:20:00 +0200

    відтепер можна вибрати місця на сеанс та передати ці данні на обробку

---
+++ Cinema/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Cinema.Data.Database;
+using Cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        [HttpPost]
+        public ActionResult BuyTickets(List<SelectedSeatsViewModel> selected_seats, int? session_id)
+        {
+            if (session_id == null || !ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            return View();
+        }
+
+        private bool ValidateSelectedSeats()
+        {
+            return true;
+        }
+    }
+}
---
+++ Cinema/Models/SelectedSeatsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+    public class SelectedSeatsViewModel
+    {
+        [Required]
+        public int Row { get; set; }
+        [Required]
+        public int Number { get; set; }
+        //public int Price { get; set; }
+    }
+}
