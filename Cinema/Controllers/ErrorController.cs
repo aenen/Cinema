93e280cbdd88c34644384c37f88de98a935df3e2
aenen <yaroslav.golyuk@yandex.ua>
2018-02-23 19:32:55 +0200

    додав контроллер з кастомними помилками (404, 400); доробив деякі маленькі сторінки

---
+++ Cinema/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+        
+
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = 400;
+            return View();
+        }
+    }
+}
